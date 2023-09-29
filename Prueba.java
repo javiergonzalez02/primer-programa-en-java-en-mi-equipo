cf64762bd534eb77f3e7bf470990dcd8acb04703
Javier González Álvarez <javiergonzalezalvarez@MacBook-Pro-de-Javier.local>
2023-09-29 13:35:39 +0200

    edad y año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba{
 
      public static void main(String args []){
-          System.out.println("González Álvarez");
+          System.out.println("González Álvarez\nTengo 21 años y nací en el 2002");
      }  
 }
