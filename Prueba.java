4291ee1b8b17b610d3ceceb9e448ee477daa33bd
javiergonzalez02 <javi.galeon2012@gmail.com>
2023-10-03 13:13:31 +0200

    Grupo de musica favorito

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba{
 
      public static void main(String args []){
-          System.out.println("Tengo 21 años y nací en el 2002\nMi comida favorita es el sushi\nMi grupo favorito son Natos y Waor");
+          System.out.println("Tengo 21 años y nací en el 2002\nMi comida favorita es el sushi\nMi grupo favorito son Natos y Waor.");
      }  
 }
